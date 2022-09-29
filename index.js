9b09aecf429f3be3d6b78fcbded97b4a3de97528
ehmaxado <eh_maxado@hotmail.com>
2022-09-28 21:33:51 -0300

    Aula 28-09-2022

--- index.js
+++ index.js
@@ -1,5 +1,50 @@
-function aplicarDesconto(valor, desconto) {
-    return valor - desconto
-}
+function validarumero(valor1){
+    
+    var result = 'falha'
+    
+    if(valor1 < 0){
+        result = 'negativo'
+    }else if (valor1 == 0){
+        result = 'zero'
+    }else if(valor1 > 0){
+        result  = 'positivo'
+    }
+    
+    return result
+    }
 
-module.exports = {aplicarDesconto}
+
+    function cordisponive(cor){
+        var retornocor = 0
+        
+        switch (cor){
+        case 'branco':
+        retornocor = 1
+        break
+        case 'azul':
+        retornocor = 1
+        break
+        case 'preto':
+        retornocor = 1
+        break
+        default:
+        retornocor = 0
+        }
+        
+        return retornocor
+        }
+
+        function numeroprimo(numero){
+        for (let i = 2; i < numero; i++)
+        if (numero % i === 0) {
+        return 1;
+        }
+        return 0;
+        }
+
+
+
+
+        module.exports = {cordisponive, validarumero, numeroprimo}
+
+        
--- index.test.js
+++ index.test.js
@@ -2,31 +2,55 @@ const test = require('tape')
 const index = require('./index')
 
 test('Teste 1', (t) => {
-    t.assert(index.aplicarDesconto(10,5) === 5,
-    "Descontou corretamente")
+    t.assert(index.validarumero(-10) === 'negativo',
+    "Validar se funciona com negativo")
     t.end()
 })
 
 test('Teste 2', (t) => {
-    t.assert(index.aplicarDesconto(5,10) === 0,
-    "Descontou corretamente")
+    t.assert(index.validarumero(0) === 'zero',
+    "Validar se funciona com zero")
     t.end()
 })
 
 test('Teste 3', (t) => {
-    t.assert(index.aplicarDesconto(8,2) === 6,
-    "Descontou corretamente")
+    t.assert(index.validarumero(100) === 'positivo',
+    "Validar se funciona com positivo")
     t.end()
 })
 
 test('Teste 4', (t) => {
-    t.assert(index.aplicarDesconto(9,2) === 6,
-    "Descontou corretamente")
+    t.assert(index.cordisponive('branco') === 1,
+    "Cor disponível")
     t.end()
 })
 
 test('Teste 5', (t) => {
-    t.assert(index.aplicarDesconto(9,2) === 7,
-    "Descontou corretamente")
+    t.assert(index.cordisponive('cinza') === 0,
+    "Cor não disponível")
+    t.end()
+})
+
+test('Teste 6', (t) => {
+    t.assert(index.cordisponive('preto') === 1,
+    "Cor disponível")
+    t.end()
+})
+
+test('Teste 7', (t) => {
+    t.assert(index.numeroprimo(11) === 0,
+    "É primo")
+    t.end()
+})
+
+test('Teste 8', (t) => {
+    t.assert(index.numeroprimo(8) === 1,
+    "não é primo")
+    t.end()
+})
+
+test('Teste 9', (t) => {
+    t.assert(index.numeroprimo(5) === 0,
+    "é primo")
     t.end()
 })
